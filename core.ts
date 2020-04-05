1a8a5f04f004e41cfc3141b19e78501ad4eebdcb
Mike Aleksiuk <maleksiuk@gmail.com>
2020-04-05 16:52:37 -0700

    Show a score.

--- core.ts
+++ core.ts
@@ -1,5 +1,57 @@
+interface ScoreTracker {
+  numberGotRight: number;
+  total: number;
+}
+
+interface Config {
+  imageHost: string;
+}
+
+let config : Config = {
+  imageHost: "hello"
+};
+
+let scoreTracker : ScoreTracker = { numberGotRight: 0, total: 0 };
+
+function showError(message: string) {
+  // mtodo
+}
+
+function updateScore() {
+  const scoreElement = document.getElementById("score");
+  if (scoreElement) {
+    scoreElement.innerText = scoreTracker.numberGotRight + "/" + scoreTracker.total;
+  } else {
+    showError("Could not update the score.");
+  }
+}
 
 function setThingsUp() {
   const pigLink = document.getElementById("pig-link");
-  console.log(pigLink);
+  const aintNoPigLink = document.getElementById("aint-no-pig-link");
+
+  if (pigLink && aintNoPigLink) {
+    pigLink.addEventListener('click', event => {
+      event.preventDefault();
+      scoreTracker.numberGotRight += 1;
+      scoreTracker.total += 1;
+      updateScore();
+      console.log('clicked on pig link');
+    });
+
+    aintNoPigLink.addEventListener('click', event => {
+      event.preventDefault();
+      scoreTracker.total += 1;
+      updateScore();
+      console.log('clicked on aint no pig link');
+    });
+
+    updateScore();
+  } else {
+    showError("Something went wrong when setting up the page. Please refresh.");
+  }
 }
+
+
+
+
